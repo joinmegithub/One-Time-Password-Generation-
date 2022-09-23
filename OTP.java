ac53de3772c51c627fdd04ffc764c616323120b4
joinmegithub <joinmegithub@gmail.com>
2022-09-23 13:47:33 +0530

    Initial commit

---
+++ OTP.java
@@ -0,0 +1,18 @@
+import java.util.Random;
+
+public class OTP {
+    public static void main(String[] args) {
+        String otp = generateOTP(6);
+        System.out.println("Generated OTP :" + otp);
+    }
+
+    public static String generateOTP(int otpLength) {
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < otpLength; i++) {
+            sb.append(random.nextInt(9));
+        }
+        String otp = sb.toString();
+        return otp;
+    }
+}
